96e911e8d9cc0ef96f7741eb539c0b3057d7c92e
ChristopherCriollo <718675@student.dusd.net>
2023-05-30 16:31:21 -0700

    Change

--- SU-57Final/Assets/Scripts/DestroyAfterDelay.cs
+++ SU-57Final/Assets/Scripts/DestroyAfterDelay.cs
@@ -7,7 +7,7 @@ public class DestroyAfterDelay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 4);
+        Destroy(gameObject, 2);
     }
 
     // Update is called once per frame
